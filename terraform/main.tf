1df9c958433f755d287778c63ba3f76caeb0112e
Ubuntu <ubuntu@ip-172-31-5-35.ap-south-1.compute.internal>
2025-12-26 04:33:20 +0000

    kubernetes and terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,97 @@
+resource "aws_vpc" "trend_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "trend-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.trend_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "trend-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "trend_igw" {
+  vpc_id = aws_vpc.trend_vpc.id
+
+  tags = {
+    Name = "trend-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.trend_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.trend_igw.id
+  }
+
+  tags = {
+    Name = "trend-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-security-group"
+  description = "Allow SSH, Jenkins and App traffic"
+  vpc_id      = aws_vpc.trend_vpc.id
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins UI"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Application Port"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "trend-jenkins-sg"
+  }
+}
+
+resource "aws_instance" "jenkins_ec2" {
+  ami                    = "ami-0f5ee92e2d63afc18"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+
+  tags = {
+    Name = "jenkins-server"
+  }
+}
+
